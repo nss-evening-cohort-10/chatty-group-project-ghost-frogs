1d743aee054cde6dd4df441ec99dd19c5f6d42a8
Randy Tate <randytatejr@gmail.com>
2019-10-19 11:07:29 -0500

    added navbar

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,4 +1,9 @@
 import 'bootstrap';
+import navbar from './components/navbar/navbar';
 import '../styles/main.scss';
 
-console.error('hi');
+const init = () => {
+  navbar.createNavbar();
+};
+
+init();
