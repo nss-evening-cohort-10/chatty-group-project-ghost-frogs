3c6c6bf528b375b9c400008a065c92c76594c2a2
Charity Bunyon <cseymou5@gmail.com>
2019-10-21 18:08:36 -0500

    fixed array and timestamp

--- src/javascripts/helpers/data/messages.js
+++ src/javascripts/helpers/data/messages.js
@@ -5,7 +5,7 @@ const messages = [
     timeStamp: 'October 19, 2019 8:02 PM',
   },
   {
-    name: 'Beetlejuice',
+    name: 'Casper',
     text: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis.',
     timeStamp: 'October 19, 2019 8:20 PM',
   },
@@ -15,7 +15,7 @@ const messages = [
     timeStamp: 'October 19, 2019 8:30 PM',
   },
   {
-    name: 'Beetlejuice',
+    name: 'Casper',
     text: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis.',
     timeStamp: 'October 19, 2019 11 PM',
   },
