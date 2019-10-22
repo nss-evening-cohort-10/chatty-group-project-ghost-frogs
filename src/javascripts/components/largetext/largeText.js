fa600800b218f603fb701ed736530535df0fd16e
Crystal Broach <crysland@gmail.com>
2019-10-21 23:44:05 -0500

    fixed background bug

--- src/javascripts/components/largetext/largeText.js
+++ src/javascripts/components/largetext/largeText.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 import './largeText.scss';
 
 const enlargeMessageText = () => {
+  console.log('clicked text check');
   const messageText = $('#chatBox');
   const cardHeading = $('h6');
   messageText.toggleClass('biggerMessageText');
